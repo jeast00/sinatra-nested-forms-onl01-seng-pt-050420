e0f7002909d2554874b27ac61286acd0fb730e6d
Jonathan East <jonathan_east@ymail.com>
2020-07-29 19:38:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,10 @@ module FormsLab
     
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
+      
+      params[:pirate][:ships].each do |details|
+        
+      end
       erb :'pirates/show'
     end
 
